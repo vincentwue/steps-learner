23e04191f4f10138e1b901486dae97b3ba756d9d
vincentwue <vini96@web.com>
2021-06-18 15:19:20 +0200

    init

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,58 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import RandomController, { IState, keys, numbers } from './Controller';
+import { classicNameResolver } from 'typescript';
+
+import classes from "./App.module.css"
 
 function App() {
+
+  const [state, setState] = useState<IState>()
+
+  const [controller] = useState(new RandomController())
+
+  useEffect(() => {
+    controller.onChange.subscribe(state => {
+      setState(state)
+    })
+  }, [])
+
+  console.log(state)
+
+  const ignore = [...keys, /* ...numbers */].map(s => <button
+    className={(state?.ignore.includes(s) ? classes.ignored : classes.included) + " " + classes.ignoreButton}
+    onClick={e => {
+      if (state) {
+
+        if (!state.ignore.includes(s)) {
+          controller.ignore = [...state.ignore, s]
+        } else {
+          controller.ignore = state.ignore.filter(item => item !== s)
+        }
+
+      }
+    }}>{s}</button>)
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div><button onClick={e => controller.nextKey()}>next key</button></div>
+
+
+      <div className={classes.box}>
+        <div className={classes.key}>{state?.key}</div>
+        <div className={classes.number}>{state?.number}</div>
+      </div>
+
+      <div><button onClick={e => {
+        if (state?.ignore.length === 0) {
+          controller.ignore = keys
+        } else {
+          controller.ignore = []
+        }
+      }}>all</button></div>
+      <div className={classes.ignore}>{ignore}</div>
+
     </div>
   );
 }
---
+++ src/Controller.ts
@@ -0,0 +1,127 @@
+import { Subject } from "rxjs"
+
+
+export const keys = [
+    "B Dur ",
+    "G# Moll",
+
+    "A Dur ",
+    "F# moll ",
+
+    "F Dur",
+    "D Moll",
+
+    "C Dur",
+    "A Moll",
+
+    "G Dur",
+    "E Moll",
+
+    "Gb Dur",
+    "Eb Moll",
+
+    "Bb Dur",
+    "G Moll",
+
+    "Eb Dur",
+    "C Moll",
+
+    "Ab Dur",
+    "F Moll",
+
+    "F Dur",
+    "Db Moll",
+
+    "Db Dur",
+    "Bb Moll",
+]
+
+export const numbers = [
+    "1",
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+]
+
+export interface IState {
+    number: string
+    key: string
+    ignore: string[]
+}
+
+export default class RandomController {
+
+    private _key = keys[1]
+    private _number = numbers[1]
+    private _ignore: string[] = []
+
+    private _onChange = new Subject<IState>()
+
+    constructor() {
+        this.nextNumber()
+        this.nextKey()
+        setInterval(() => {
+            this.nextKey()
+        }, 10000)
+        setInterval(() => {
+            this.nextNumber()
+        }, 2000)
+    }
+
+
+
+    private publish() {
+        this._onChange.next({
+            number: this._number,
+            key: this._key,
+            ignore: this._ignore
+        })
+    }
+
+    public nextNumber() {
+        const n = randomFromArray(numbers.filter(n => n !== this._number).filter(n => !this._ignore.includes(n)))
+        if (!n) {
+            this.nextNumber()
+            return
+        }
+        this._number = n
+        this.publish()
+    }
+
+    public nextKey() {
+        let k = undefined
+        let i = 0
+        while (!k && i<100) {
+            k = randomFromArray(keys.filter(n => n !== this._key).filter(n => !this._ignore.includes(n)))
+            i++
+        }
+        if (k) {
+            this._key = k
+            this.publish()
+        }
+
+    }
+
+    public get ignore() {
+        return this._ignore
+    }
+
+    public set ignore(arr: string[]) {
+        this._ignore = arr
+        this.publish()
+    }
+
+
+    public get onChange() {
+        return this._onChange
+    }
+
+
+}
+
+function randomFromArray(arr: string[]) {
+    return arr[Math.floor((Math.random() * arr.length))];
+}
